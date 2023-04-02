e48eba2f772cd199b294622b3aa709a7cdf6d25e
Constantine <kosta.tovstik@gmail.com>
2023-04-03 01:40:44 +0300

    Added more validation

--- library/src/main/java/ua/sirkostya009/library/config/SecurityConfig.java
+++ library/src/main/java/ua/sirkostya009/library/config/SecurityConfig.java
@@ -3,18 +3,17 @@ package ua.sirkostya009.library.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.security.interfaces.RSAPublicKey;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -37,14 +36,20 @@ public class SecurityConfig {
 
     @Bean
     public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withPublicKey(publicKey).build();
+        var decoder = NimbusJwtDecoder.withPublicKey(publicKey).build();
+        decoder.setJwtValidator(delegatingValidator());
+        return decoder;
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(JwtDecoder jwtDecoder) {
-        return new ProviderManager(
-                new JwtAuthenticationProvider(jwtDecoder)
+    private OAuth2TokenValidator<Jwt> delegatingValidator() {
+        return new DelegatingOAuth2TokenValidator<>(
+                new JwtTimestampValidator(),
+                audienceValidator()
         );
+    }
+
+    private OAuth2TokenValidator<Jwt> audienceValidator() {
+        return new JwtClaimValidator<List<String>>("aud", audience -> audience.contains("/library/"));
     }
 
 }
