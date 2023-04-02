ed4f57526c168a3df3112dc3bcc0daa81dd1d409
Constantine <kosta.tovstik@gmail.com>
2023-04-02 17:35:43 +0300

    Removed password from User

--- ui/src/main/java/ua/sirkostya009/ui/model/User.java
+++ ui/src/main/java/ua/sirkostya009/ui/model/User.java
@@ -3,7 +3,6 @@ package ua.sirkostya009.ui.model;
 public record User(
         String email,
         String username,
-        String password,
         String firstName,
         String lastName
 ) {
